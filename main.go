8d0c1ad567a417718498c459fa530d04354e0690
Nikita Plotnikov <nikita@codnity.com>
2025-12-07 15:06:15 +0200

    Build character by character

--- main.go
+++ main.go
@@ -1,42 +1,50 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
-	"strconv"
 )
 
+type Options struct {
+	length uint
+	includeNumbers bool
+  includeLower bool
+	includeUpper bool
+}
+
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("Usage: main.go <length>")
-		return
+	lengthPtr := flag.Uint("length", 0, "Length of the output (required)")
+	includeNumbersPtr := flag.Bool("numbers", false, "Include numbers (0-9)")
+	includeLowerPtr := flag.Bool("lower", false, "Include lowercase letters (a-z)")
+	includeUpperPtr := flag.Bool("upper", false, "Include uppercase letters (A-Z)")
+	flag.Parse()
+
+	if *lengthPtr == 0 {
+		fmt.Println("Error with input: --length is a required argument.")
+		flag.Usage()
+		os.Exit(1)
 	}
-	length := os.Args[1]
+	if !*includeNumbersPtr && !*includeLowerPtr && !*includeUpperPtr {
+		fmt.Print("Error: at least one character set should be included.")
+		fmt.Println("Include either -numbers, -lower, or -upper flag.")
+		flag.Usage()
+		os.Exit(1)
+	}
+	// check if len is less then required sets
 
-	val, err := Validate(length)
-	if err != nil {
-		fmt.Printf("Error with value input: %v\n", err)
-		return
+	options := Options{
+		length: *lengthPtr,
+		includeNumbers: *includeNumbersPtr,
+		includeLower: *includeLowerPtr,
+		includeUpper: *includeUpperPtr,
 	}
 
-	charset := []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
-
-	password, err := GeneratePassword(charset, val)
+	password, err := GenPwd(options)
 	if err != nil {
 		fmt.Printf("Error with password generation: %v\n", err)
-		return
+		os.Exit(1)
 	}
 
 	fmt.Printf("Password: %q\n", password)
-}
-
-func Validate(length string) (int, error) {
-	val, err := strconv.Atoi(length)
-	if err != nil {
-		return 0, fmt.Errorf("conversion error: %v", err)
-	}
-	if val <= 0 {
-		return 0, fmt.Errorf("parameter(%v) must be a positive integer", length)
-	}
-	return val, nil
 }
--- pass.go
+++ pass.go
@@ -2,28 +2,78 @@ package main
 
 import (
 	"crypto/rand"
+	mathrand "math/rand"
 	"fmt"
 	"math/big"
 	"slices"
 )
 
-func GeneratePassword(charset []byte, length int) (string, error) {
-	charsetInitialLength := len(charset)
-	if charsetInitialLength < length {
-		return "", fmt.Errorf("desired length  %v exceeds available characters %v", length, charsetInitialLength)
+func GenRndRune(charset []rune) ([]rune, rune, error) {
+	bigIdx, err := rand.Int(rand.Reader, big.NewInt(int64(len(charset))))
+	if err != nil {
+		return []rune("0"), 0, fmt.Errorf("error from rand.Int dependency %v", err)
 	}
+	idx := int(bigIdx.Int64())
+	result := charset[idx]
+	newCharset := append(charset[:idx], charset[idx + 1:]...)
+	return newCharset, result, nil
+}
 
-	password := make([]rune, length)
-
-	for i := range password {
-		bigIdx, err := rand.Int(rand.Reader, big.NewInt(int64(len(charset))))
+func GenPwd(options Options) (string, error) {
+	var charset []rune
+	var charsetCounter int
+	var result []rune
+	if options.includeNumbers {
+		charsetNum := []rune("0123456789")
+		newCharset, n, err := GenRndRune(charsetNum)
 		if err != nil {
-			return "", fmt.Errorf("error from rand.Int dependency %v", err)
+			return "", fmt.Errorf("error while generating numbers %v", err)
 		}
-		idx := int(bigIdx.Int64())
-		password[i] = rune(charset[idx])
-		charset = slices.Delete(charset, idx, idx + 1)
+		result = append(result, n)
+
+		charset = slices.Concat(charset, newCharset)
+		charsetCounter++
 	}
-	result := string(password)
-	return result, nil
+	if options.includeLower {
+		charsetLower := []rune("abcdefghijklmnopqrstuvwxyz")
+		newCharset, n, err := GenRndRune(charsetLower)
+		if err != nil {
+			return "", fmt.Errorf("error while generating numbers %v", err)
+		}
+		result = append(result, n)
+
+		charset = slices.Concat(charset, newCharset)
+		charsetCounter++
+	}
+	if options.includeUpper {
+		charsetUpper := []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+		newCharset, n, err := GenRndRune(charsetUpper)
+		if err != nil {
+			return "", fmt.Errorf("error while generating numbers %v", err)
+		}
+		result = append(result, n)
+
+		charset = slices.Concat(charset, newCharset)
+		charsetCounter++
+	}
+
+	if charsetCounter > int(options.length) {
+		return "", fmt.Errorf("length is too small to satisfy one character from each set rule" )
+	}
+	if int(options.length - uint(charsetCounter)) > len(charset) {
+		return "", fmt.Errorf("length is too big to satisfy unique characters rule" )
+	}
+	var char rune
+	var err error
+	for range options.length - uint(charsetCounter) {
+		charset, char, err = GenRndRune(charset)
+		if err != nil {
+			return "", fmt.Errorf("error while generating rest %v", err)
+		}
+		result = append(result, char)
+	}
+
+	mathrand.Shuffle(len(result), func(i, j int) { result[i], result[j] = result[j], result[i] })
+
+	return string(result), nil
 }
