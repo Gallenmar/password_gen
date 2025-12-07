06f646c998e0e02ba0a89448ce2be0fd1374a72e
Nikita Plotnikov <nikita@codnity.com>
2025-12-07 16:42:31 +0200

    Time the function

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"os"
+	"time"
 )
 
 type Options struct {
@@ -14,37 +14,24 @@ type Options struct {
 }
 
 func main() {
-	lengthPtr := flag.Uint("length", 0, "Length of the output (required)")
-	includeNumbersPtr := flag.Bool("numbers", false, "Include numbers (0-9)")
-	includeLowerPtr := flag.Bool("lower", false, "Include lowercase letters (a-z)")
-	includeUpperPtr := flag.Bool("upper", false, "Include uppercase letters (A-Z)")
-	flag.Parse()
-
-	if *lengthPtr == 0 {
-		fmt.Println("Error with input: --length is a required argument.")
-		flag.Usage()
-		os.Exit(1)
-	}
-	if !*includeNumbersPtr && !*includeLowerPtr && !*includeUpperPtr {
-		fmt.Print("Error: at least one character set should be included.")
-		fmt.Println("Include either -numbers, -lower, or -upper flag.")
-		flag.Usage()
-		os.Exit(1)
-	}
-	// check if len is less then required sets
-
 	options := Options{
-		length: *lengthPtr,
-		includeNumbers: *includeNumbersPtr,
-		includeLower: *includeLowerPtr,
-		includeUpper: *includeUpperPtr,
+		length: 4,
+		includeNumbers: true,
+		includeLower: true,
+		includeUpper: true,
 	}
+
+	start := time.Now()
 
 	password, err := GenPwd(options)
 	if err != nil {
 		fmt.Printf("Error with password generation: %v\n", err)
 		os.Exit(1)
 	}
+
+	elapsed := time.Since(start)
+	fmt.Printf("Time: %s\n", elapsed)
+
 
 	fmt.Printf("Password: %q\n", password)
 }
