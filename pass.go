b341256a21fac26fde02b4922ac7b6525343cc74
Nikita Plotnikov <nikita@codnity.com>
2025-12-07 16:30:13 +0200

    Shuffle the shuffle

--- pass.go
+++ pass.go
@@ -5,73 +5,79 @@ import (
 	mathrand "math/rand"
 	"fmt"
 	"math/big"
-	"slices"
 )
 
-func GenRndRune(charset []rune) ([]rune, rune, error) {
+func GenRndRune(charset []rune) (rune, error) {
 	bigIdx, err := rand.Int(rand.Reader, big.NewInt(int64(len(charset))))
 	if err != nil {
-		return []rune("0"), 0, fmt.Errorf("error from rand.Int dependency %v", err)
+		return 0, fmt.Errorf("error from rand.Int dependency %v", err)
 	}
 	idx := int(bigIdx.Int64())
 	result := charset[idx]
-	newCharset := append(charset[:idx], charset[idx + 1:]...)
-	return newCharset, result, nil
+	return result, nil
 }
 
 func GenPwd(options Options) (string, error) {
-	var charset []rune
-	var charsetCounter int
+	charset := make(map[rune]bool)
+	var charsetCounter uint
 	var result []rune
 	if options.includeNumbers {
-		charsetNum := []rune("0123456789")
-		newCharset, n, err := GenRndRune(charsetNum)
+		charsetNumString := "0123456789"
+		for i := range charsetNumString {
+			charset[rune(charsetNumString[i])] = false
+		}
+		n, err := GenRndRune([]rune(charsetNumString))
 		if err != nil {
 			return "", fmt.Errorf("error while generating numbers %v", err)
 		}
 		result = append(result, n)
-
-		charset = slices.Concat(charset, newCharset)
+		delete(charset, n)
 		charsetCounter++
 	}
 	if options.includeLower {
 		charsetLower := []rune("abcdefghijklmnopqrstuvwxyz")
-		newCharset, n, err := GenRndRune(charsetLower)
+		for i := range charsetLower {
+			charset[rune(charsetLower[i])] = false
+		}
+		n, err := GenRndRune([]rune(charsetLower))
 		if err != nil {
 			return "", fmt.Errorf("error while generating numbers %v", err)
 		}
 		result = append(result, n)
-
-		charset = slices.Concat(charset, newCharset)
+		delete(charset, n)
 		charsetCounter++
 	}
 	if options.includeUpper {
 		charsetUpper := []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-		newCharset, n, err := GenRndRune(charsetUpper)
+		for i := range charsetUpper {
+			charset[rune(charsetUpper[i])] = false
+		}
+		n, err := GenRndRune([]rune(charsetUpper))
 		if err != nil {
 			return "", fmt.Errorf("error while generating numbers %v", err)
 		}
 		result = append(result, n)
-
-		charset = slices.Concat(charset, newCharset)
+		delete(charset, n)
 		charsetCounter++
 	}
 
-	if charsetCounter > int(options.length) {
-		return "", fmt.Errorf("length is too small to satisfy one character from each set rule" )
+	if charsetCounter > options.length {
+		return "", fmt.Errorf("length is too small to satisfy one character from each set rule")
 	}
-	if int(options.length - uint(charsetCounter)) > len(charset) {
-		return "", fmt.Errorf("length is too big to satisfy unique characters rule" )
+	if int(options.length - charsetCounter) > len(charset) {
+		return "", fmt.Errorf("length is too big to satisfy unique characters rule (max: %v)", len(charset)+int(charsetCounter))
 	}
-	var char rune
-	var err error
-	for range options.length - uint(charsetCounter) {
-		charset, char, err = GenRndRune(charset)
-		if err != nil {
-			return "", fmt.Errorf("error while generating rest %v", err)
-		}
-		result = append(result, char)
+	keys := make([]rune, len(charset))
+	i := 0
+	for k := range charset {
+			keys[i] = k
+			i++
 	}
+
+	mathrand.Shuffle(len(keys), func(i, j int) { keys[i], keys[j] = keys[j], keys[i] })
+
+	rest := options.length-charsetCounter
+	result = append(result, keys[:rest]...)
 
 	mathrand.Shuffle(len(result), func(i, j int) { result[i], result[j] = result[j], result[i] })
 
